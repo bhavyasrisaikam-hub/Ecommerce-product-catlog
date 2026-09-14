37120798d4f609d201953193986e0819f3839f70
bhavyasrisaikam-hub <bhavyasri.saikam@gmail.com>
2026-09-14 20:24:26 +0530

    rewards.js

--- rewards.js
+++ rewards.js
@@ -397,7 +397,7 @@ function displayRewardHistory() {
                 <h3>🎁 ${item.title}</h3>
 
                 <p>
-                    💰 Savings: ₹${item.savings}
+                    💰 Savings: ₹${item.discount}
                 </p>
 
                 <p>
